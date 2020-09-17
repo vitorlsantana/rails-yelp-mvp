7b0a527126180af746a5de1ea68cdce36e3c7770
Vitor Santana <santana.vitor@gmail.com>
2020-09-17 16:46:38 -0300

    rails new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+category = ["chinese", "italian", "japanese", "french", "belgian"]
+
+15.times  do
+  Restaurant.create!(name: Faker::Restaurant.name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.cell_phone, category: category.sample)
+end
